7a33522770fb2861f12ef33a2e54f1619ee9cafa
Amin Mirzaei <aminmirzaeidev@gmail.com>
2026-01-04 02:20:51 +0330

    Update DeveloperPage.Designer.cs

--- PingPong/Pages/DeveloperPage.Designer.cs
+++ PingPong/Pages/DeveloperPage.Designer.cs
@@ -66,7 +66,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(394, 66);
             this.button2.TabIndex = 4;
-            this.button2.Text = "Github : AminMirzaeiOne";
+            this.button2.Text = "Github : aminmirzaei-dev";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
